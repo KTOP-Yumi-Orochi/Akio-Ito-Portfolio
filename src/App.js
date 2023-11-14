048310813e4d3e0a591308bfb1d1242aa9f98297
KTOP-Yumi-Orochi <yumiorochi712@gmail.com>
2023-11-14 18:00:40 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import "./App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import EngSharedData from './utils/portfolio_shared_data.json';
+import JapSharedData from './utils/portfolio_shared_data-jp.json';
+import EngResumeData from './utils/res_primaryLanguage.json';
+import JapResumeData from './utils/res_secondaryLanguage.json';
+
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      foo: "bar",
+      resumeData: {},
+      sharedData: {},
+    };
+  }
+
+  applyPickedLanguage(pickedLanguage, oppositeLangIconId) {
+    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+    document.documentElement.lang = pickedLanguage;
+    var resumePath =
+      document.documentElement.lang === window.$primaryLanguage
+        ? true
+        : false;
+    this.loadResumeFromPath(resumePath);
+    this.loadSharedDataFromPath(resumePath);
+  }
+
+  swapCurrentlyActiveLanguage(oppositeLangIconId) {
+    var pickedLangIconId =
+      oppositeLangIconId === window.$primaryLanguageIconId
+        ? window.$secondaryLanguageIconId
+        : window.$primaryLanguageIconId;
+    document
+      .getElementById(oppositeLangIconId)
+      .removeAttribute("filter", "brightness(40%)");
+    document
+      .getElementById(pickedLangIconId)
+      .setAttribute("filter", "brightness(40%)");
+  }
+
+  componentDidMount() {
+    this.applyPickedLanguage(
+      window.$primaryLanguage,
+      window.$secondaryLanguageIconId
+    );
+  }
+
+  loadResumeFromPath(path) {
+    if(path === true) {
+      this.setState({ resumeData: EngResumeData });
+    }
+    else {
+      this.setState({ resumeData: JapResumeData });
+    }
+  }
+
+  loadSharedDataFromPath(path) {
+    if(path === true) {
+      this.setState({ sharedData: EngSharedData });
+    }
+    else {
+      this.setState({ sharedData: JapSharedData });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header sharedData={this.state.sharedData.basic_info} />
+        <div className="col-md-12 mx-auto text-center language">
+          <div
+            onClick={() =>
+              this.applyPickedLanguage(
+                window.$primaryLanguage,
+                window.$secondaryLanguageIconId
+              )
+            }
+            style={{ display: "inline" }}
+          >
+            <span
+              className="iconify language-icon mr-5"
+              data-icon="twemoji-flag-for-flag-united-states"
+              data-inline="false"
+              id={window.$primaryLanguageIconId}
+            ></span>
+          </div>
+          <div
+            onClick={() =>
+              this.applyPickedLanguage(
+                window.$secondaryLanguage,
+                window.$primaryLanguageIconId
+              )
+            }
+            style={{ display: "inline" }}
+          >
+            <span
+              className="iconify language-icon"
+              data-icon="twemoji-flag-for-flag-japan"
+              data-inline="false"
+              id={window.$secondaryLanguageIconId}
+            ></span>
+          </div>
+        </div>
+        <About
+          resumeBasicInfo={this.state.resumeData.basic_info}
+          sharedBasicInfo={this.state.sharedData.basic_info}
+        />
+        <Projects
+          resumeProjects={this.state.resumeData.projects}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Skills
+          sharedSkills={this.state.sharedData.skills}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Experience
+          resumeExperience={this.state.resumeData.experience}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+      </div>
+    );
+  }
+}
+
+export default App;
